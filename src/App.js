42721debd451d21d7a83754ddb8a4f3d396dc050
aarturio <106455653+aarturio@users.noreply.github.com>
2023-02-28 23:42:18 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedBackStats";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    setFeedback(feedback.filter((item) => item.id !== id));
+  };
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
+}
+export default App;
