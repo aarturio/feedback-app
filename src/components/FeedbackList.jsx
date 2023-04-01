74c762f5a776cc502a45a337921978de896bb218
aarturio <106455653+aarturio@users.noreply.github.com>
2023-04-01 15:58:48 -0400

    added json server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,18 @@ import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "./context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if ((!feedback || feedback.length === 0) & !isLoading) {
     return <p>No feedback yet</p>;
   }
-  return (
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -20,6 +24,7 @@ function FeedbackList() {
       </AnimatePresence>
     </div>
   );
+
   // return (
   //   <div className="feedback-list">
   //     {feedback.map((item) => (
